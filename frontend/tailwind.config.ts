3098da15c8219d07f7c9cf85a2626f5bd419ec67
arimeltzer <31070449+arimeltzer@users.noreply.github.com>
2026-06-26 20:49:25 -0400

    Update tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,6 +2,19 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+      fontSize: {
+        xs:   ['0.8125rem', { lineHeight: '1.25rem' }],
+        sm:   ['0.9375rem', { lineHeight: '1.5rem'  }],
+        base: ['1.0625rem', { lineHeight: '1.75rem' }],
+        lg:   ['1.1875rem', { lineHeight: '1.75rem' }],
+        xl:   ['1.3125rem', { lineHeight: '1.875rem'}],
+      },
+    },
+  },
   plugins: [],
 } satisfies Config;
